35b597a3dfb8fa396b1a5172d37d37209f048107
liam <terroridk@gmail.com>
2020-12-10 15:47:00 -0500

    [setup] initial boilerplate + setup code

---
+++ InventoryTracker/Models/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryTracker.Models {
+    public class Inventory {
+        ///<summary> Total cost of all items in the inventory. </summary>
+        private double value = 0;
+
+        ///<summary> Total money made from selling items from the inventory. </summary>
+        private double revenue = 0;
+
+        ///<summary> List of all items in the inventory. </summary>
+        private List<Item> items;
+
+        ///<summary> Generates an inventory with an empty list of items. </summary>
+        public Inventory()
+        {
+            items = new List<Item>();
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public void LoadFile(string fileLocation)
+        {
+            throw new NotImplementedException("yeet");
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public void SaveFile(string fileLocation)
+        {
+            throw new NotImplementedException("yeet");
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public void Sort()
+        {
+            throw new NotImplementedException("yeet");
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public string GenerateReport()
+        {
+            throw new NotImplementedException("yeet");
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public void AddItems(List<Item> newItems)
+        {
+            // just add to items list. dont forget update value
+            throw new NotImplementedException("yeet");
+        }
+
+        ///<summary> DESCRIPTION BLABLABLA </summary>
+        public void SellItems(List<Item> soldItems)
+        {
+            // just remove form list.dont forget update revenue
+            throw new NotImplementedException("yeet");
+        }
+    }
+}
---
+++ InventoryTracker/Models/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryTracker.Models {
+    class Item {
+        private int quantity;
+        private double cost;
+        private int optimalQuantity;
+
+        public Item(int quantity, double cost, int optimalQuantity, string category, string supplier, string location) {
+            this.quantity = quantity;
+            this.cost = cost;
+            this.optimalQuantity = optimalQuantity;
+            Category = category;
+            Supplier = supplier;
+            Location = location;
+        }
+
+        public int Quantity {
+            get { return quantity; }
+            set {
+                if (value < 0) 
+                    throw new ArgumentException("Quantity cannot be less than 0.");
+                quantity = value;
+            }
+        }
+
+        public double Cost {
+            get { return cost; }
+            set {
+                if (value < 0)
+                    throw new ArgumentException("Cost cannot be less than 0.");
+                cost = value;
+            }
+        }
+
+        public int OptimalQuantity {
+            get { return optimalQuantity; }
+            set {
+                if (value < 0) 
+                    throw new ArgumentException("Optimal Quantity cannot be less than 0.");
+                quantity = value;
+            }
+        }
+
+        public string Supplier { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+    }
+}
